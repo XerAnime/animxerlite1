8ab882f2bae2130996079b3f5b69e0878eb2fc4b
XerAnime <160598198+XerAnime@users.noreply.github.com>
2024-03-04 23:40:00 +0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -6,7 +6,7 @@ const recentapi = "/recent/";
 
 // Api Server Manager
 
-const AvailableServers = ['https://api1.anime-dex.workers.dev', 'https://api2.anime-dex.workers.dev', 'https://api3.anime-dex.workers.dev']
+const AvailableServers = ['https://animedexapi.xeranime707.workers.dev/', 'https://animedexapi.xeranime707.workers.dev/', 'https://animedexapi.xeranime707.workers.dev/']
 
 function getApiServer() {
     return AvailableServers[Math.floor(Math.random() * AvailableServers.length)]
