06ca8e38d1165d12573b0508d2c292ec02ebd13d
XerAnime <160598198+XerAnime@users.noreply.github.com>
2024-03-04 23:38:30 +0800

    Change API☑️

--- js/anime.js
+++ js/anime.js
@@ -7,9 +7,9 @@ const recommendationsapi = "/recommendations/";
 // Api Server Manager
 
 const AvailableServers = [
-    "https://api1.anime-dex.workers.dev",
-    "https://api2.anime-dex.workers.dev",
-    "https://api3.anime-dex.workers.dev",
+    "https://animedexapi.xeranime707.workers.dev/",
+    "https://animedexapi.xeranime707.workers.dev/",
+    "https://animedexapi.xeranime707.workers.dev/",
 ];
 
 function getApiServer() {
