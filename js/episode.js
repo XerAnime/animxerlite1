3825dd87d7753a2bd765657e6786df678befa60b
XerAnime <160598198+XerAnime@users.noreply.github.com>
2024-03-25 13:51:10 +0800

    Update episode.js

--- js/episode.js
+++ js/episode.js
@@ -10,7 +10,7 @@ const dlapi = "/download/";
 const AvailableServers = [
     "https://animedexapi.rexcalumba86.workers.dev",
     "https://animedexapi.xeranime707.workers.dev",
-    "https://animedexapi.rexcalumba86.workers.dev",
+    "https://animxer-api1.xeranime707.workers.dev",
 ];
 
 function getApiServer() {
