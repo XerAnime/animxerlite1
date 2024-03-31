eb19bfba046b5f1de4d23991c1ed8e5e9546ea23
XerAnime <160598198+XerAnime@users.noreply.github.com>
2024-03-31 10:35:00 +0800

    Update episode.js

--- js/episode.js
+++ js/episode.js
@@ -7,7 +7,7 @@ const dlapi = "/download/";
 
 // Api Server Manager
 
-const AvailableServers = ["api.xeranime707.workers.dev",];
+const AvailableServers = ["https://animxer-api1.xeranime707.workers.dev",];
 
 function getApiServer() {
     return AvailableServers[Math.floor(Math.random() * AvailableServers.length)];
