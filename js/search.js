bf2ae8a2fc5076b404b067d35d40097624004e53
XerAnime <160598198+XerAnime@users.noreply.github.com>
2024-03-31 15:36:04 +0800

    Update search.js

--- js/search.js
+++ js/search.js
@@ -5,7 +5,7 @@ const searchapi = "/search/";
 
 // Api Server Manager
 
-const AvailableServers = ["api.animxersite.workers.dev"];
+const AvailableServers = ["https://api.druvx13.workers.dev"];
 
 function getApiServer() {
     return AvailableServers[Math.floor(Math.random() * AvailableServers.length)];
