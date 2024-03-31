10e819412130038df8139244ac62b06a428b6795
XerAnime <160598198+XerAnime@users.noreply.github.com>
2024-03-31 14:56:35 +0800

    Update anime.js

--- js/anime.js
+++ js/anime.js
@@ -6,7 +6,7 @@ const recommendationsapi = "/recommendations/";
 
 // Api Server Manager
 
-const AvailableServers = ["https://api.druvx13.workers.dev"];
+const AvailableServers = ["api.animxersite.workers.dev"];
 
 function getApiServer() {
     return AvailableServers[Math.floor(Math.random() * AvailableServers.length)];
