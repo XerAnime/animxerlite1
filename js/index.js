9b57deb02d622edd6fab9ce57f2665e943b87348
XerAnime <160598198+XerAnime@users.noreply.github.com>
2024-06-01 23:04:34 +0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -6,7 +6,7 @@ const recentapi = "/recent/";
 
 // Api Server Manager
 
-const AvailableServers = ["https://api.druvx13.workers.dev"];
+const AvailableServers = ["https://api3.xeranime707.workers.dev"];
 
 function getApiServer() {
     return AvailableServers[Math.floor(Math.random() * AvailableServers.length)];
